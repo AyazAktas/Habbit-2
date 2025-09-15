d7ac3bff0fd7f7fe1f532b5d791f328d0c5e5e65
Ayaz Aktaş <ayazaktastr@gmail.com>
2025-09-15 18:31:28 +0300

    adding singelton structure to database

--- app/src/main/java/com/example/habbit/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/habbit/data/local/AppDatabase.kt
@@ -1,12 +1,32 @@
 package com.example.habbit.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.habbit.data.local.dao.HabitDao
 import com.example.habbit.data.local.entity.Habit
 
 
 @Database(entities = [Habit::class], version = 1, exportSchema = true)
+@TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun habitDao(): HabitDao
+    companion object{
+        @Volatile
+        private var INSTANCE: AppDatabase?=null
+
+        fun getInstance(context: Context): AppDatabase{
+            return INSTANCE?:synchronized(this){
+                val instance= Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "habbit_db"
+                ).build()
+                INSTANCE=instance
+                instance
+            }
+        }
+    }
 }
