11cd0eb4f5eb10887aedb31c776d4a4007634023
Ayaz Aktaş <ayazaktastr@gmail.com>
2025-09-19 11:44:36 +0300

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.ayaz.habbit"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
